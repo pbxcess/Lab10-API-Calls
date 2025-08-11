79a77ceb91ec388a503fb05b95c91784669ba01b
Princess Krish Anne Bernardino <pkabernardino@gmail.com>
2025-08-11 12:20:05 -0400

    Completing Task 1: API Interaction using GET requests

--- script.js
+++ script.js
@@ -3,3 +3,21 @@ const fetchBtn = document.getElementById('fetchBtn');
 const xhrBtn = document.getElementById('xhrBtn');
 const postForm = document.getElementById('postForm');
 const putForm = document.getElementById('putForm');
+
+//to Display messages
+function displayMessage(message, isError = false) {
+    output.innerHTML = `<p style="color:${isError ? 'red' : 'green'}">${message}</p>`;
+}
+
+//Task 1
+fetchBtn.addEventListener('click', () => {
+    fetch('https://jsonplaceholder.typiode.come/posts/1')
+        .then(res => {
+            if (!res.ok) throw new Error(`HTTP error! Status: ${res.status}`);
+            return res.json();
+        })
+        .then(data => {
+            output.innerHTML = `<h3>${data.title}</h3><p>${data.body}</p>`;
+        })
+        .catch(err => displayMessage(`Fetch error: ${err.message}`, true));
+})
