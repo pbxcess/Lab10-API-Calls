d6b4e0c7811cc00aba907dd93580aa255bddb490
Princess Krish Anne Bernardino <pkabernardino@gmail.com>
2025-08-11 12:34:03 -0400

    Amending Typos for Task 2 and 3

--- script.js
+++ script.js
@@ -23,8 +23,8 @@ fetchBtn.addEventListener('click', () => {
 });
 
 //Task 2
-xhrBtn.addEventListener('click', => {
-    const.xhr = new XMLHttpRequest();
+xhrBtn.addEventListener('click', () => {
+    const xhr = new XMLHttpRequest();
     xhr.open('GET', 'https://jsonplaceholder.typicode.com/posts/2');
     xhr.onload = function() {
         if (xhr.status >= 200 && xhr.status < 300) {
