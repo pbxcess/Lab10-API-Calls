bd50f6895af11a147b8150a0de23989dd2429fbc
Princess Krish Anne Bernardino <pkabernardino@gmail.com>
2025-08-11 12:11:53 -0400

    Setting up variable declarations for DOM elements

---
+++ script.js
@@ -0,0 +1,5 @@
+const output = document.getElementById('output');
+const fetchBtn = document.getElementById('fetchBtn');
+const xhrBtn = document.getElementById('xhrBtn');
+const postForm = document.getElementById('postForm');
+const putForm = document.getElementById('putForm');
